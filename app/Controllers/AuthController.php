bbb27344b412da8c1fadba06f77b2caeb7d6128e
Tariqul Islam ronnie <tariqul.islam.rony@gmail.com>
2017-04-11 01:47:14 +0600

    Add the Payload feature to json web token

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -9,12 +9,21 @@ class AuthController extends Controller{
  public function index($request,$response){
     $key = "keyofslimstarterkit";
 
-    $token = array(
-    "iss" => "http://example.org",
-    "aud" => "http://example.com",
-    "iat" => 1356999524,
-    "nbf" => 1357000000
-     );
+    $token = [
+        "alg"=> "HS256",
+        "iss" => "http://example.org", //The issuer of the token
+        "aud" => "http://example.com", //The audience of the token
+        "iat" => 1356999524,
+        "nbf" => 1357000000,
+        "typ"=> "JWT",
+        "payload"=> [
+
+            "sub"=> "1234567890",
+            "name"=> "John Doe",
+            "admin"=> true
+
+        ]
+     ];
      $jwt = JWT::encode($token, $key);
 
      return $jwt;
